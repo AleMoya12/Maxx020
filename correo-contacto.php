7b218d8c48d232dc278aa6b681b12c26736d46bb
alemoya12 <alemoya5@gmail.com>
2022-07-02 04:18:40 -0300

    Maxx020

---
+++ correo-contacto.php
@@ -0,0 +1,10 @@
+<?php
+    $destino= "infomaxx020@gmail.com
+    ";
+    $nombre = $_POST["name"];
+    $correo = $_POST["email"];
+    $mensaje = $_POST["messaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje . "\nDesea Info sobre tus servicios ";
+    mail($destino, "Nueva consulta desde Sitio Web", $contenido);
+    header("Location:index.html");
+?>
